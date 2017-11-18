b86a16e8de804234838de69f4432e327773b5ac8
Chumister <league@wts22.attlocal.net>
2017-11-18 15:58:35 -0800

    8

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,41 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
+		// 3. Change this line to give you a random number between 1 - 100.
 		int random = new Random().nextInt(5);
 		// 2. Print out the random variable above
-		
+
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
+		for (int i = 0; i < 11; i++) {
 
-			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
-			// 5. if the guess is correct
-				// 6. win
-			// 7. if the guess is high
-				// 8. tell them it's too high
-			// 9. if the guess is low
-				// 10. tell them it's too low
+		}
+		// 1. ask the user for a guess using a pop-up window, and save their response
+		String a = JOptionPane.showInputDialog("");
+		// 4. convert the users’ answer to an int (Integer.parseInt(string))
+		int c = Integer.parseInt(a);
+		// 5. if the guess is correct
+		// 6. win\
+		if (c == 9) {
 
-		// 12. tell them they lose
+			JOptionPane.showConfirmDialog(null, "You win!");
+
+		}
+		// 7. if the guess is high
+		// 8. tell them it's too high
+		if (c > 9) {
+			JOptionPane.showConfirmDialog(null, "Too High!");
+
+		}
+		// 9. if the guess is low
+		if (c < 9) {
+			// 10. tell them it's too low
+			JOptionPane.showConfirmDialog(null, "Too low!");
+			// 12. tell them they lose
+		}
 	}
-
 }
-
-
--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,16 +3,23 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
-	public static void main(String[] args) {
-		
-		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
 
- // 1. Use the dance method to make the robot spin.
-	
+	public static void main(String[] args) {
+
+		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number
+		// of times.
+		String a = JOptionPane.showInputDialog("How much do you want the robot to spin?");
+		// 1. Use the dance method to make the robot spin.
+		int c = Integer.parseInt(a);
+		robot.setSpeed(10);
+
+		dance(10);
 
 	}
 
@@ -22,4 +29,3 @@ public class DizzyRobot {
 		}
 	}
 }
-
